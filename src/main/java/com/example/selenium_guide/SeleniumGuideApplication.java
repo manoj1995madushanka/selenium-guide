15666ddb21ce31307a5a0e94e519924bae512a62
manoj1995madushanka <madushankamanoj414@gmail.com>
2025-01-17 22:25:18 +0530

    init

---
+++ src/main/java/com/example/selenium_guide/SeleniumGuideApplication.java
@@ -0,0 +1,13 @@
+package com.example.selenium_guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeleniumGuideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/selenium_guide/SeleniumGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.selenium_guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
