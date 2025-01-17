48d48231de789517ee249b1615fd88bda2a9d5bd
manoj1995madushanka <madushankamanoj414@gmail.com>
2025-01-17 23:09:03 +0530

    demo 1

--- src/main/java/com/example/selenium_guide/SeleniumGuideApplication.java
+++ src/main/java/com/example/selenium_guide/SeleniumGuideApplication.java
@@ -1,13 +1,39 @@
 package com.example.selenium_guide;
 
-import org.springframework.boot.SpringApplication;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SeleniumGuideApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SeleniumGuideApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // Set the property for webdriver.chrome.driver to be the location to your local download of chromedriver
+        System.setProperty(
+                "webdriver.chrome.driver",
+                "D:\\course\\selenium\\selenium-guide\\src\\main\\resources\\chromedriver\\chromedriver.exe"
+        );
+
+        // Create new instance of ChromeDriver
+        WebDriver driver = new ChromeDriver();
+
+        // And now use this to visit Google
+        driver.get("http://www.google.com");
+
+        // Find the text input element by its name
+        WebElement element = driver.findElement(By.name("q"));
+
+        // Enter something to search for
+        element.sendKeys("Cheese!");
+
+        // Now submit the form
+        element.submit();
+
+        //Close the browser
+        //driver.quit();
+
+    }
 
 }
