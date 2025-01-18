966343cfb2849757dc514618564442b6c6862ac3
manoj1995madushanka <madushankamanoj414@gmail.com>
2025-01-18 09:40:10 +0530

    scrolling example

---
+++ src/main/java/com/example/selenium_guide/ScrollToElement.java
@@ -0,0 +1,31 @@
+package com.example.selenium_guide;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+/**
+ * Example to scroll webpage down and select element
+ */
+public class ScrollToElement {
+    public static void main(String[] args) {
+        System.setProperty(
+                "webdriver.chrome.driver",
+                "D:\\course\\selenium\\selenium-guide\\src\\main\\resources\\chromedriver\\chromedriver.exe"
+        );
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://formy-project.herokuapp.com/scroll");
+
+        WebElement name = driver.findElement(By.id("name"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(name); // move to page down until it meets name element
+        name.sendKeys("Manoj");
+
+        WebElement date = driver.findElement(By.id("date"));
+        date.sendKeys("18/01/2025");
+
+        driver.quit();
+    }
+}
